0ec2a2dad9eb5a90a9929a196ae9ff054a10cf5b
Arushi Kathpal <arushikathpal@gmail.com>
2019-10-20 12:59:29 -0400

    WIP : Search and Get Info for a specific User
    Populating View User Info page with all details specific to a user based on search criteria -- In progress

---
+++ app/public/api/records/getInfo.php
@@ -0,0 +1,22 @@
+<?php
+// Step 1: Get a datase connection from our help class
+$db = DbConnection::getConnection();
+// Step 2: Create & run the query
+if (isset($_GET['firstName'])) {
+  $stmt = $db->prepare(
+    'SELECT * FROM People WHERE firstName = ?'
+  );
+  $stmt->execute([$_GET['firstName']]);
+}
+//why??
+else {
+  $stmt = $db->prepare('SELECT * FROM People join CertDetails where People.personID=CertDetails.personID');
+  $stmt->execute();
+}
+$people = $stmt->fetchAll();
+
+// Step 3: Convert to JSON
+$json = json_encode($people, JSON_PRETTY_PRINT);
+// Step 4: Output
+header('Content-Type: application/json');
+echo $json;
