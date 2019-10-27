d9d98ce72da504aa696b777d1b3bc81fd2108b80
Arushi Kathpal <arushikathpal@gmail.com>
2019-10-27 14:41:21 -0400

    Full functionality of filter by Cert Name
    Enter name of cert and fetch new result set

--- app/public/js/expCert.app.js
+++ app/public/js/expCert.app.js
@@ -1,40 +1,58 @@
 var expCertApp = new Vue({
   el: '#expiredCerts',
   data: {
-    expcerts: []
-
+    getName: '',
+    expcerts: [],
+    expcertts1:[]
   },
   methods: {
     fetchexpcerts() {
       fetch('api/records/expiredCerts.php')
       .then(response => response.json())
       .then(json => {expCertApp.expcerts = json})
-      }
-    },
-/*
-    handleSubmit(event) {
-      fetch('api/records/post.php', {
-        method:'POST',
-        body: JSON.stringify(this.recordPatient),
-        headers: {
-          "Content-Type": "application/json; charset=utf-8"
+      },
+
+    specificCert()
+    {
+        console.log("currently me expcerts array has:")
+        console.log(this.expcerts);
+        console.log("Got name of cert");
+        console.log(this.getName);
+        this.expcerts1=this.expcerts;
+        console.log("Pushed to new array, new array as follows");
+        console.log(this.expcerts1);
+        console.log("Emptying array now..");
+        this.expcerts=[];
+
+        console.log("Does my new array have content?");
+        console.log(this.expcerts1);
+        console.log("Contents of old array");
+        console.log(this.expcerts);
+
+        // for(var u of this.users)
+        // {
+        //   if(this.searchName == u.firstName)
+
+      for(var e of this.expcerts1)
+      {
+        console.log("My e is..")
+        console.log( e);
+
+        if(this.getName == e.certName)
+        {
+          this.expcerts.push(e);
         }
-      })
-
-      .then(response => response.json())
-      .then(json => { patientRecordsApp.patients.push(json[0]) })
-
-      this.handleReset();
-    },
-    handleReset() {
-      this.recordPatient = {
-        firstName: ''
       }
+      console.log("my filtered array is..");
+      console.log(this.expcerts);
+      }
+
     },
-    handleRowClick(patient) {
-      patientTriageApp.patient = patient;
-    }
-  }, // end methods */
+
+
+
+
+
   created() {
     this.fetchexpcerts();
   }
